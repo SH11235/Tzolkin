6f073c6cebfa5c39a85dae413feba7f274a4c2fe
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2023-02-21 21:58:17 +0900

    struct gameのfield修正

--- src/game_object/game.rs
+++ src/game_object/game.rs
@@ -1,33 +1,39 @@
+pub(crate) const FIRST_FOOD_DAY: u32 = 8;
+pub(crate) const SECOND_FOOD_DAY: u32 = 14;
+pub(crate) const THIRD_FOOD_DAY: u32 = 21;
+pub(crate) const FOURTH_FOOD_DAY: u32 = 27;
+
 #[derive(Debug)]
 pub struct Round(pub u32);
-#[derive(Debug)]
-pub struct FoodDay(pub u32);
+impl ValidRound for Round {
+    fn is_valid(&self) -> bool {
+        self.0 >= 1 && self.0 <= FOURTH_FOOD_DAY
+    }
+}
+pub trait Increment {
+    fn increment(&mut self);
+}
+impl Increment for u32 {
+    fn increment(&mut self) {
+        *self += 1;
+    }
+}
+impl Increment for Round {
+    fn increment(&mut self) {
+        if self.0 < FOURTH_FOOD_DAY {
+            self.0.increment();
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Generation(pub u32);
 pub trait ValidRound {
     fn is_valid(&self) -> bool;
 }
-
-pub trait ValidFoodDay {
-    fn is_valid(&self) -> bool;
-}
-
 pub trait ValidGeneration {
     fn is_valid(&self) -> bool;
 }
-
-impl ValidRound for Round {
-    fn is_valid(&self) -> bool {
-        self.0 >= 1 && self.0 <= 27
-    }
-}
-
-impl ValidFoodDay for FoodDay {
-    fn is_valid(&self) -> bool {
-        self.0 == 8 || self.0 == 14 || self.0 == 21 || self.0 == 27
-    }
-}
-
 impl ValidGeneration for Generation {
     fn is_valid(&self) -> bool {
         self.0 == 1 || self.0 == 2
@@ -35,56 +41,33 @@ impl ValidGeneration for Generation {
 }
 
 #[derive(Debug)]
-pub(crate) struct Game<T: ValidRound, F: ValidFoodDay, G: ValidGeneration> {
+pub(crate) struct Game<T: ValidRound, G: ValidGeneration> {
     pub round: T,
     pub turn_player: String,
-    pub food_days: Vec<F>,
+    pub food_days: [u32; 4],
     pub generation: G,
-    pub corn_stock: u32,
 }
 
-impl<T: ValidRound, F: ValidFoodDay, G: ValidGeneration> Game<T, F, G> {
-    pub fn new(
-        round: T,
-        turn_player: String,
-        food_days: Vec<F>,
-        generation: G,
-        corn_stock: u32,
-    ) -> Result<Self, &'static str> {
+impl<T: ValidRound, G: ValidGeneration> Game<T, G> {
+    const FOOD_DAYS: [u32; 4] = [
+        FIRST_FOOD_DAY,
+        SECOND_FOOD_DAY,
+        THIRD_FOOD_DAY,
+        FOURTH_FOOD_DAY,
+    ];
+    pub fn new(round: T, turn_player: String, generation: G) -> Result<Self, &'static str> {
         if !round.is_valid() {
             return Err("Invalid round value");
         }
         if !generation.is_valid() {
             return Err("Invalid generation value");
         }
-        for food_day in &food_days {
-            if !food_day.is_valid() {
-                return Err("Invalid food day value");
-            }
-        }
         Ok(Self {
             round,
             turn_player,
-            food_days,
+            food_days: Self::FOOD_DAYS,
             generation,
-            corn_stock,
         })
-    }
-}
-
-pub trait Increment {
-    fn increment(&mut self);
-}
-
-impl Increment for u32 {
-    fn increment(&mut self) {
-        *self += 1;
-    }
-}
-
-impl Increment for Round {
-    fn increment(&mut self) {
-        self.0.increment();
     }
 }
 
@@ -94,14 +77,7 @@ mod tests {
 
     #[test]
     fn test_increase_round() {
-        let mut game = Game::new(
-            Round(1),
-            "Alice".to_string(),
-            vec![FoodDay(8), FoodDay(14), FoodDay(21)],
-            Generation(1),
-            100,
-        )
-        .unwrap();
+        let mut game = Game::new(Round(1), "Alice".to_string(), Generation(1)).unwrap();
         assert_eq!(game.round.0, 1);
         game.round.increment();
         assert_eq!(game.round.0, 2);
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
 mod game_object;
 
-use game_object::game::{FoodDay, Game, Generation, Increment, Round};
-
-// enum Position {
-//     Hand,
-//     Palenque(u32),
-//     Yaxchilan(u32),
-//     Tikal(u32),
-//     Uxmal(u32),
-//     ChichenItza(u32),
-//     StartPlayer,
-//     // QuickActions,
-// }
+use game_object::game::{Game, Generation, Increment, Round};
 
 // struct Jungle {
 //     second_corn_tiles: u32,
@@ -23,44 +12,7 @@ use game_object::game::{FoodDay, Game, Generation, Increment, Round};
 //     fifth_corn_tiles: u32,
 // }
 
-// struct Worker {
-//     position: Position,
-// }
-
-// struct Technology {
-//     agriculture: u32,
-//     resource: u32,
-//     construction: u32,
-//     temple: u32,
-// }
-
-// struct Resource {
-//     woods: u32,
-//     stones: u32,
-//     golds: u32,
-// }
-
-// struct Player {
-//     name: String,
-//     order: u32,
-//     workers: Vec<Worker>,
-//     technology: Technology,
-//     corns: u32,
-//     resource: Resource,
-//     skulls: u32,
-//     corn_tiles: u32,
-//     wood_tiles: u32,
-//     points: u32,
-// }
-
 fn main() {
-    let mut game = Game::new(
-        Round(1),
-        "Alice".to_string(),
-        vec![FoodDay(8), FoodDay(14), FoodDay(21)],
-        Generation(1),
-        100,
-    )
-    .unwrap();
+    let mut game = Game::new(Round(1), "Alice".to_string(), Generation(1)).unwrap();
     game.round.increment();
 }
