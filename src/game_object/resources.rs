db6300d77438780ab8de9b5c2bd2aa68d938838c
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2023-02-23 21:56:55 +0900

    fix 資源の換算ロジック修正

--- src/game_object/resources.rs
+++ src/game_object/resources.rs
@@ -1,33 +1,33 @@
 pub trait Resource {
-    fn convert_to_points(&self) -> f32;
+    fn convert_to_corns_rate(&self) -> u32;
 }
 
 #[derive(Debug, Default)]
 pub struct Wood(pub u32);
 impl Resource for Wood {
-    fn convert_to_points(&self) -> f32 {
-        self.0 as f32 / 2.0
+    fn convert_to_corns_rate(&self) -> u32 {
+        self.0 * 2
     }
 }
 #[derive(Debug, Default)]
 pub struct Stone(pub u32);
 impl Resource for Stone {
-    fn convert_to_points(&self) -> f32 {
-        self.0 as f32 * 3.0 / 4.0
+    fn convert_to_corns_rate(&self) -> u32 {
+        self.0 * 3
     }
 }
 #[derive(Debug, Default)]
 pub struct Gold(pub u32);
 impl Resource for Gold {
-    fn convert_to_points(&self) -> f32 {
-        self.0 as f32
+    fn convert_to_corns_rate(&self) -> u32 {
+        self.0 * 4
     }
 }
 #[derive(Debug, Default)]
 pub struct Skull(pub u32);
-impl Resource for Skull {
-    fn convert_to_points(&self) -> f32 {
-        self.0 as f32 * 3.0
+impl Skull {
+    pub fn convert_to_points(&self) -> u32 {
+        self.0 * 3
     }
 }
 
