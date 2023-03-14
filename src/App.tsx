319f890d8f73f45fb2248ac109347e7de34cf36f
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2023-03-15 00:05:08 +0900

    design logo変更

--- src/App.tsx
+++ src/App.tsx
@@ -19,24 +19,18 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
       <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://ja.boardgamearena.com/gamepanel?game=tzolkin" target="_blank">
+          <img src="/gear_logo.png" className="logo gear" alt="Gear logo" />
         </a>
       </div>
 
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
       <div className="row">
-        <img src="/gear_beige.png" alt="gear" />
+        <div className="field-container">
+
+        </div>
         <span className="temples">
           <Temple
             name="Chaac"
